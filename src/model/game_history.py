18cf1d05416caede135ed841a6ddb8280b767353
Adam Polak <pl.adam.polak@gmail.com>
2013-01-07 20:56:39 +0100

    Implemented GameHistory class.

--- src/model/game_history.py
+++ src/model/game_history.py
@@ -1,12 +1,25 @@
 class GameHistory(object):
+    def __init__(self):
+        self._moves = []
 
-  def __init__(self):
-    self._turns = 0
+    def append_move(self, move):
+        """
+        Appends move if it is valid. Returns True on success.
+        """
+        cur_state = self.get_state_after_move(self.get_num_moves())
+        if move.validate(cur_state):
+            _moves.append(move)
+            return True
+        else:
+            return False
 
-  def get_turns(self):
-    return self._turns
+    def get_num_moves(self):
+        return len(self._moves)
 
-  def get_state_after_turn(self, turn):
-    assert 0 <= turn and turn <= self._turns
-    return None
+    def get_state_after_move(self, move_id):
+        assert 0 <= move_id and move_id <= len(self._moves)
+        state = GameState() #TODO: how to initialize game state?
+        for move in self._moves[:move_id]:
+            move.make(state)
+        return state
 
