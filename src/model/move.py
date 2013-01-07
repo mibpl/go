835417c76be30ba87179f14553123b83e82de03c
Adam Polak <pl.adam.polak@gmail.com>
2013-01-07 20:31:41 +0100

    Modified Move interface.

--- src/model/move.py
+++ src/model/move.py
@@ -1,12 +1,19 @@
 class Move(object):
-
-    def validate(self, game_history):
-        pass
-
-    def make(self, game_history):
+    def validate(self, game_state):
         """
-        First checks if `validate` was called; if not - throws.
-        Returns GameState object - result of applying the move to the last state
-        kept in game_history.
+        Returns true iff this is a valid move for a given game state.
         """
         pass
+
+    def make(self, game_state):
+        """
+        Applies this move to game_state. Assumes this is a valid move.
+        """
+        pass
+
+    def validate_and_make(self, game_state):
+        if not self.validate(game_state):
+            return False
+        else:
+            self.make(game_state)
+            return True
--- src/model/place_stone_move.py
+++ src/model/place_stone_move.py
@@ -1,2 +1,14 @@
+from move import Move
+
 class PlaceStoneMove(Move):
-  pass
+    def __init__(self, row, column, token):
+        self._row = row
+        self._column = column
+        self._token
+
+    def validate(self, game_state):
+        return True
+
+    def make(self, game_state):
+        game_state.get_board().set_token(row, column, token)
+
