8e5fe78a3f408d829c9081fa7a3f76aaa65a6350
mib <mib@citadel.(none)>
2013-01-07 18:24:52 +0100

    Wypisywanie i dodatkowy assert w BoardModel wq

--- src/model/board_model.py
+++ src/model/board_model.py
@@ -1,5 +1,7 @@
 class BoardModel(object):
 
+    TOKENS = ['empty', 'white', 'black']
+
     def __init__(self, size):
         self._size = size
         self._M = [["empty" for i in range(0, size)] for j in range(0, size)]
@@ -8,6 +10,7 @@ class BoardModel(object):
         return self._size
 
     def set_token(self, row, column, token):
+        assert token in BoardModel.TOKENS
         assert 0 <= row and row < self._size 
         assert 0 <= column and column < self._size
         self._M[row][column] = token
@@ -17,10 +20,16 @@ class BoardModel(object):
         assert 0 <= column and column < self._size
         return self._M[row][column]
 
+    def __unicode__(self):
+        return u'\n'.join([' '.join([f[0] for f in row]) for row in self._M])
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
 def test():
-        b = BoardModel(3)
-        b.set_token(0, 0, "white")
-        b.set_token(2, 2, "black")
-        assert b.get_token(0, 0) == "white"
-        assert b.get_token(1, 1) == "empty"
-        assert b.get_token(2, 2) == "black"
+    b = BoardModel(3)
+    b.set_token(0, 0, "white")
+    b.set_token(2, 2, "black")
+    assert b.get_token(0, 0) == "white"
+    assert b.get_token(1, 1) == "empty"
+    assert b.get_token(2, 2) == "black"
