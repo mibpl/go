63276cf13f4eb44891bb52ea974ae99df40bd9ff
Jakub Adamek <jakub.adamek@uj.edu.pl>
2013-01-08 15:58:41 +0100

    Implement missing methods in controller

--- src/go/controller/controller.py
+++ src/go/controller/controller.py
@@ -1,32 +1,50 @@
-from ..model.moves import PlaceStoneMove
+from ..model.moves import PlaceStoneMove, PassMove
 
 class Controller(object):
     def __init__(self, game_history):
         self._history = game_history
         self._time = 0
+        self._current_time = 0
         self._view = None
 
     def set_view(self, view):
         self._view = view
         self._update_view()
 
-    def click(self, row, column):
-        state = self._get_current_state()
-        player = state.active_player
-        move = PlaceStoneMove(row, column, player)
-        # TODO: validate move
+    def _is_current(self):
+        return self._current_time == self._time
+
+    def _is_start(self):
+        return self._current_time == 0
+
+    def _execute_move(self, move):
         self._history.append_move(move)
         self._time += 1
         self._update_view()
 
+    def click(self, row, column):
+        if not self._is_current():
+            pass
+        state = self._get_current_state()
+        player = state.active_player
+        move = PlaceStoneMove(row, column, player)
+        self._execute_move(move)
+
     def do_pass(self):
-        pass
+        if not self._is_current():
+            pass
+        move = PassMove()
+        self._execute_move(move)
 
     def navigate_prev(self):
-        pass
+        if self._is_start():
+            pass
+        self._current_time -= 1
 
     def navigate_next(self):
-        pass
+        if self._is_current():
+            pass
+        self._current_time += 1
 
     def _get_current_state(self):
         return self._history.get_state_after_move(self._time)
--- src/go/controller/test_controller.py
+++ src/go/controller/test_controller.py
@@ -2,7 +2,7 @@
 import unittest
 import mock
 from .controller import Controller
-from ..model.moves import PlaceStoneMove
+from ..model.moves import PlaceStoneMove, PassMove
 
 class TestBoardModel(unittest.TestCase):
 
@@ -13,6 +13,15 @@ class TestBoardModel(unittest.TestCase):
         controller.set_view(view_mock)
         controller.click(4, 4)
         game_history_mock.append_move.assert_called()
+        view_mock.set_game_state.assert_called()
+
+    def test_do_pass(self):
+        game_history_mock = mock.Mock()
+        view_mock = mock.Mock()
+        controller = Controller(game_history_mock)
+        controller.set_view(view_mock)
+        controller.do_pass()
+        game_history_mock.append_move.assert_called(PassMove())
         view_mock.set_game_state.assert_called()
 
 if __name__ == '__main__':
